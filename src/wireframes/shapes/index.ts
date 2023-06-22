e34efe22bf4c3e52cd8e38c4290e8d07cf4451f2
uplight-dev <12542203+uplight-dev@users.noreply.github.com>
2023-06-22 04:34:15 +0200

    Update index.ts

--- src/wireframes/shapes/index.ts
+++ src/wireframes/shapes/index.ts
@@ -6,7 +6,8 @@
 */
 
 import { RendererService } from '@app/wireframes/model/renderer.service';
-import { Browser, Button, ButtonBar, Checkbox, ComboBox, Comment, Dropdown, Grid, Heading, HorizontalLine, HorizontalScrollbar, Icon, Image, Label, Link, List, Numeric, Paragraph, Phone, Progress, RadioButton, Raster, Rectangle, Shape, Slider, Tablet, Tabs, TextArea, TextInput, Toggle, VerticalLine, VerticalScrollbar, Window } from './dependencies';
+//import { Browser, Button, ButtonBar, Checkbox, ComboBox, Comment, Dropdown, Grid, Heading, HorizontalLine, HorizontalScrollbar, Icon, Image, Label, Link, List, Numeric, Paragraph, Phone, Progress, RadioButton, Raster, Rectangle, Shape, Slider, Tablet, Tabs, TextArea, TextInput, Toggle, VerticalLine, VerticalScrollbar, Window } from './dependencies';
+import { Image } from './dependencies';
 import { AbstractControl } from './utils/abstract-control';
 
 export function registerRenderers() {
